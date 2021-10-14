6421e498b05ec272b130a631e39c90893e66fe52
akile99 <akile99@gmail.com>
2021-10-14 14:53:03 -0400

    adding menu and directory compenents with some scss.

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./menu-items.styles.scss";
+
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div
+    style={{ backgroundImage: `url(${imageUrl}` }}
+    className={`${size} menu-item`}
+  >
+    <div className="content">
+      <h1 className="title">{title}</h1>
+      <span className="subtitle">SHOPT NOW</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
