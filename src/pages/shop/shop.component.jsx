5d6217daa914690d4b009c5b986ce561af0f1730
akile99 <akile99@gmail.com>
2021-10-15 23:38:08 -0400

    I added a sign in page with a custom form and created a custom button component.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import SHOP_DATA from "./shop.data";
-import ComponentPreview from "../../components/collection-preview/collection-preview.component";
+// import ComponentPreview from "../../components/collection-preview/collection-preview.component";
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
 class ShopPage extends React.Component {
