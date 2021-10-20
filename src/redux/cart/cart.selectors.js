7d1c5c6cf37b622aeaff02340abc01d1a72988fd
akile99 <akile99@gmail.com>
2021-10-20 10:45:23 -0400

    added reselect and implemented createStructorSelectory

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -7,6 +7,11 @@ export const selectCartItems = createSelector(
   (cart) => cart.cartItems
 );
 
+export const selectCartHidden = createSelector(
+  [selectCart],
+  (cart) => cart.hideCartDropdown
+);
+
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
   (cartItems) =>
