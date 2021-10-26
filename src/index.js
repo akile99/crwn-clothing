706e9ced14ce98cd3b7e76b0d3d12346f519331f
akile99 <akile99@gmail.com>
2021-10-26 17:45:03 -0400

    added redux for STORE_DATA collections and for our directory.

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/es/integration/react";
 
 import "./index.css";
 import App from "./App";
+import Loading from "./components/loading/loading.component";
 
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate loading={<Loading />} persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
